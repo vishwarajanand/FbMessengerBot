9f97b7e5bb084d1e8fa3d2064a8343e5cf537f99
Vishwaraj Anand <7369612+vishwarajanand@users.noreply.github.com>
2020-04-08 22:02:41 +0800

    First commit - not working as yet

---
+++ config.js
@@ -0,0 +1,9 @@
+const APPID = process.env.APPID;
+const PAGEID = process.env.PAGEID;
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
+
+module.exports = {
+  APPID,
+  PAGEID,
+  PAGE_ACCESS_TOKEN,
+};
